e78b2b66af4bb34619397bef6a9f1dc4ec34d655
ss108 <samier_saeed@protonmail.com>
2023-01-15 11:58:38 -0500

    installed and set up prettier

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,21 @@
-import { BaseEditor, Descendant } from "slate";
-import { ReactEditor, RenderElementProps, RenderLeafProps } from "slate-react";
+import {BaseEditor, Descendant} from "slate";
+import {ReactEditor, RenderElementProps, RenderLeafProps} from "slate-react";
 
 export enum TRACK_CHANGES_OPERATION {
-    TEXT_ADDED = 0,
-    TEXT_DELETED = 1,
-    RESTYLED = 2,
+  TEXT_ADDED = 0,
+  TEXT_DELETED = 1,
+  RESTYLED = 2,
 }
 
 export type TrackChangesInfo = {
-    operation: TRACK_CHANGES_OPERATION;
-    changes?: any[]; //arbitrary information describing changes, meant for the consuming codebase to take action upon
+  operation: TRACK_CHANGES_OPERATION;
+  changes?: any[]; //arbitrary information describing changes, meant for the consuming codebase to take action upon
 };
 
 export type EditorMode = "TrackChanges"; //should this concept be kept, or just made a normal boolean field of TrackChangesEditor?
-export type Modes = { [key in EditorMode]?: boolean };
+export type Modes = {[key in EditorMode]?: boolean};
 
-export type TrackChangesEditor = BaseEditor & ReactEditor & {
+export type TrackChangesEditor = BaseEditor &
+  ReactEditor & {
     modes: Modes;
-}
+  };
--- src/withTrackChanges.ts
+++ src/withTrackChanges.ts
@@ -1,5 +1,18 @@
-import { Editor } from "slate";
+import {Editor} from "slate";
+import {TrackChangesEditor} from "types";
 
-export function withTrackChanges<T extends Editor>(e: T) {
+export function withTrackChanges<T extends Editor>(e: T): TrackChangesEditor {
+  const {
+    deleteBackward,
+    deleteForward,
+    insertText,
+    deleteFragment,
+    insertFragment,
+  } = e;
+  const editor = e as T & TrackChangesEditor;
 
+  if (!editor.modes) {
+    editor.modes = {TrackChanges: false};
+    console.log("Initialized editor modes");
+  }
 }

1fca725ae0114924bce58f63970970fbaac48e74
ss108 <samier_saeed@protonmail.com>
2023-01-15 11:15:35 -0500

    installed slate packages; added some type definitions

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import { BaseEditor, Descendant } from "slate";
+import { ReactEditor, RenderElementProps, RenderLeafProps } from "slate-react";
+
+export enum TRACK_CHANGES_OPERATION {
+    TEXT_ADDED = 0,
+    TEXT_DELETED = 1,
+    RESTYLED = 2,
+}
+
+export type TrackChangesInfo = {
+    operation: TRACK_CHANGES_OPERATION;
+    changes?: any[]; //arbitrary information describing changes, meant for the consuming codebase to take action upon
+};
+
+export type EditorMode = "TrackChanges"; //should this concept be kept, or just made a normal boolean field of TrackChangesEditor?
+export type Modes = { [key in EditorMode]?: boolean };
+
+export type TrackChangesEditor = BaseEditor & ReactEditor & {
+    modes: Modes;
+}
---
+++ src/withTrackChanges.ts
@@ -0,0 +1,5 @@
+import { Editor } from "slate";
+
+export function withTrackChanges<T extends Editor>(e: T) {
+
+}
